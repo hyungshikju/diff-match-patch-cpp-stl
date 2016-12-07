8e935f9acecac37c02cbe8bc3c08b26e4c232f92
Dongeun Kim <deiori44@gmail.com>
2016-12-07 13:25:01 +0900

    fixed #4 retry

--- diff_match_patch_test_string.cpp
+++ diff_match_patch_test_string.cpp
@@ -21,7 +21,7 @@
  * http://code.google.com/p/google-diff-match-patch/
  */
 // pull request 테스트용 소스 수정
-//#include "diff_match_patch.h"
+#include "diff_match_patch.h"
 #include <cstring>
 #include <iomanip>
 #include <iostream>
@@ -42,7 +42,7 @@ struct wastring : wstring  // The same as wstring, but can be constructed from c
   wastring operator+=(const char* s) { append(s, s + strlen(s)); return *this; }
 
   static const wchar_t eol = L'\n';
-  static const wchar_t tab = L'\t';  
+  static const wchar_t tab = L'\t';
 };
 
 inline wastring operator+(const wastring& s, const char* p) { return s + wastring(p); }
